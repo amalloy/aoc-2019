661f9823f9513eb315339069893a6365407a84b7
Alan Malloy <alan@malloys.org>
2019-11-30 21:57:18 -0800

    Day 1, part 2

--- day1/src/Main.hs
+++ day1/src/Main.hs
@@ -3,13 +3,18 @@ module Main where
 import Control.Arrow ((&&&))
 
 fuelCost :: Int -> Int
-fuelCost size = (size `div` 3) - 2
+fuelCost mass = max 0 $ (mass `div` 3) - 2
 
 part1 :: [Int] -> Int
 part1 = sum . map fuelCost
 
-part2 :: [Int] -> ()
-part2 s = ()
+tyrannicalCost :: Int -> [Int]
+tyrannicalCost 0 = []
+tyrannicalCost mass = fuel : tyrannicalCost fuel
+  where fuel = fuelCost mass
+
+part2 :: [Int] -> Int
+part2 = sum . concatMap tyrannicalCost
 
 main :: IO ()
 main = interact $ show . (part1 &&& part2) . map read . lines
