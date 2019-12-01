a22e38fefa6dd85881ece8cb6a165a3acd42565b
Alan Malloy <alan@malloys.org>
2019-11-30 22:12:15 -0800

    Day 1, part 2: use recursion schemes

--- day1/src/Main.hs
+++ day1/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.Functor.Foldable (ana, ListF(..))
 
 fuelCost :: Int -> Int
 fuelCost mass = max 0 $ (mass `div` 3) - 2
@@ -13,8 +14,14 @@ tyrannicalCost 0 = []
 tyrannicalCost mass = fuel : tyrannicalCost fuel
   where fuel = fuelCost mass
 
+tyrannicalCost' :: Int -> [Int]
+tyrannicalCost' = ana f
+  where f 0 = Nil
+        f mass = Cons fuel fuel
+          where fuel = fuelCost mass
+
 part2 :: [Int] -> Int
-part2 = sum . concatMap tyrannicalCost
+part2 = sum . concatMap tyrannicalCost'
 
 main :: IO ()
 main = interact $ show . (part1 &&& part2) . map read . lines
