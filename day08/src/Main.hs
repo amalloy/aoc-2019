2b1d10330b9d4997ef5e45c5d6e3db684783f8f4
Alan Malloy <alan@malloys.org>
2019-12-07 22:12:05 -0800

    Day 8, part 2

--- day08/src/Main.hs
+++ day08/src/Main.hs
@@ -1,27 +1,31 @@
 module Main where
 
 import Control.Arrow ((&&&))
-import Data.List (minimumBy)
+import Data.List (minimumBy, transpose)
 import Data.List.Split (chunksOf)
 import Data.Ord (comparing)
 
-type Input = String
+type Input = [String]
 
 width, height :: Int
 width = 25
 height = 6
 
 part1 :: Input -> Int
-part1 s = let layers = chunksOf (width * height) s
-              count n = (length . filter (== n))
-              bestLayer = minimumBy (comparing (count '0')) layers
+part1 layers = let count n = (length . filter (== n))
+                   bestLayer = minimumBy (comparing (count '0')) layers
           in count '1' bestLayer * count '2' bestLayer
 
-part2 :: Input -> ()
-part2 i = ()
+part2 :: Input -> String
+part2 layers = let pixels = transpose layers
+                   image = map (head . dropWhile (== '2')) pixels
+               in unlines . chunksOf width $ image
 
 prepare :: String -> Input
-prepare = init
+prepare = chunksOf (width * height) . init
 
 main :: IO ()
-main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
+main = do
+  input <- prepare <$> readFile "input.txt"
+  print $ part1 input
+  putStr $ part2 input
