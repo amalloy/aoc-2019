a746de6d83efd51d9e0a6a2f812c20fc4a1f7930
Alan Malloy <alan@malloys.org>
2019-12-07 22:00:17 -0800

    Day 8, part 1

--- day08/src/Main.hs
+++ day08/src/Main.hs
@@ -1,17 +1,27 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.List (minimumBy)
+import Data.List.Split (chunksOf)
+import Data.Ord (comparing)
 
-type Input = [String]
+type Input = String
 
-part1 :: Input -> ()
-part1 i = ()
+width, height :: Int
+width = 25
+height = 6
+
+part1 :: Input -> Int
+part1 s = let layers = chunksOf (width * height) s
+              count n = (length . filter (== n))
+              bestLayer = minimumBy (comparing (count '0')) layers
+          in count '1' bestLayer * count '2' bestLayer
 
 part2 :: Input -> ()
 part2 i = ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = init
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
