10c98e55bb6e90dc5350d4ba4f7cdabee8a7d4b4
Alan Malloy <alan@malloys.org>
2019-12-08 22:31:45 -0800

    Day 9, part 2

--- day09/src/Main.hs
+++ day09/src/Main.hs
@@ -4,16 +4,16 @@ import Advent.Intcode
 
 import Control.Arrow ((&&&))
 
-type Input = Computer
+type Input = Program
 
 part1 :: Input -> Either String [Value]
-part1 = fmap outputs . runToCompletion
+part1 = fmap outputs . runToCompletion . (`mkComputer` [1])
 
-part2 :: Input -> ()
-part2 i = ()
+part2 :: Input -> Either String [Value]
+part2 = fmap outputs . runToCompletion . (`mkComputer` [2])
 
 prepare :: String -> Input
-prepare = (`mkComputer` [1]) . parseIntcodeProgram
+prepare = parseIntcodeProgram
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
